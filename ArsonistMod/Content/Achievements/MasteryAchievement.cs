28f18e32a8e4808c3b52b19fa24764d8982775b0
Ethanol10 <35375774+Ethanol10@users.noreply.github.com>
2022-10-27 00:38:13 +1100

    Renaming stuff

---
+++ ArsonistMod/Content/Achievements/MasteryAchievement.cs
@@ -0,0 +1,19 @@
+﻿using RoR2;
+using System;
+using UnityEngine;
+
+namespace ArsonistMod.Modules.Achievements
+{
+    internal class MasteryAchievement : BaseMasteryUnlockable
+    {
+        public override string AchievementTokenPrefix => ArsonistPlugin.DEVELOPER_PREFIX + "_ARSONIST_BODY_MASTERY";
+        //the name of the sprite in your bundle
+        public override string AchievementSpriteName => "texMasteryAchievement";
+        //the token of your character's unlock achievement if you have one
+        public override string PrerequisiteUnlockableIdentifier => ArsonistPlugin.DEVELOPER_PREFIX + "_ARSONIST_BODY_UNLOCKABLE_REWARD_ID";
+
+        public override string RequiredCharacterBody => "ArsonistBody";
+        //difficulty coeff 3 is monsoon. 3.5 is typhoon for grandmastery skins
+        public override float RequiredDifficultyCoefficient => 3;
+    }
+}
---
+++ ArsonistMod/Modules/States.cs
@@ -0,0 +1,22 @@
+﻿using ArsonistMod.SkillStates;
+using ArsonistMod.SkillStates.BaseStates;
+using System.Collections.Generic;
+using System;
+
+namespace ArsonistMod.Modules
+{
+    public static class States
+    {
+        internal static void RegisterStates()
+        {
+            Modules.Content.AddEntityState(typeof(BaseMeleeAttack));
+            Modules.Content.AddEntityState(typeof(SlashCombo));
+
+            Modules.Content.AddEntityState(typeof(Shoot));
+
+            Modules.Content.AddEntityState(typeof(Roll));
+
+            Modules.Content.AddEntityState(typeof(ThrowBomb));
+        }
+    }
+}
