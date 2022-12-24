2f106e5dc2a115b7fd997c1f85780975c4424513
Ethanol10 <35375774+Ethanol10@users.noreply.github.com>
2022-12-24 21:45:40 +1100

    preparing onhit controllers

---
+++ ArsonistMod/Modules/ProjectileControllers/StrongFiresprayOnHit.cs
@@ -0,0 +1,36 @@
+﻿using RoR2.Projectile;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace ArsonistMod.Modules.ProjectileControllers
+{
+    internal class StrongFiresprayOnHit : MonoBehaviour, IProjectileImpactBehavior
+    {
+        public void Awake()
+        {
+
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void FixedUpdate() 
+        {
+            
+        }
+
+        public void OnDestroy() 
+        {
+            
+        }
+
+        public void OnProjectileImpact(ProjectileImpactInfo impactInfo)
+        {
+
+        }
+    }
+}
