0895cd00ea6688ad743041ab6cbc536d7944fd51
Ethanol10 <35375774+Ethanol10@users.noreply.github.com>
2023-02-18 21:46:46 +1100

    err sfx I think

---
+++ ArsonistMod/Content/Controllers/ArsonistDisplayController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace ArsonistMod.Content.Controllers
+{
+    internal class ArsonistDisplayController : MonoBehaviour
+    {
+        internal float stopwatch;
+        internal static float igniteFraction;
+
+        void Start() 
+        {
+            stopwatch = 0f;
+        }
+
+        void Update()
+        {
+            //Update certain parameters here, the ignite animation is playing at the start so time effects and such 
+            //To this animation at 1x speed.
+
+
+        }
+    }
+}
