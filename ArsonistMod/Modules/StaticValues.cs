abdbf7d73ca30bd3dc752d3740633ce4264a2445
Ethan Goh <ethangoh98@gmail.com>
2023-06-23 22:45:43 +1000

    Update StaticValues.cs

--- ArsonistMod/Modules/StaticValues.cs
+++ ArsonistMod/Modules/StaticValues.cs
@@ -105,6 +105,7 @@ namespace ArsonistMod.Modules
         internal static float masochismFinalBlastRadius = 12f;
         internal static float masochismDamageMultiplierPerStack = 1.5f;
         internal static float masochismMaxMultipliedRange = 3f;
+        internal static float masochismFOVHoldPosition = 1.5f;
 
         internal static float masochismMoveSpeedPenalty = 0.7f;
         internal static float masochismDamagePenalty = 0.7f;
