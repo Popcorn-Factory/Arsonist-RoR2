eba5d2bdf3ffb96b7861f59242681a0ad60345fd
Ethanol10 <35375774+Ethanol10@users.noreply.github.com>
2023-08-11 16:24:14 +1000

    Update FlareEffectControllerStrong.cs

--- ArsonistMod/SkillStates/FlareEffectControllerStrong.cs
+++ ArsonistMod/SkillStates/FlareEffectControllerStrong.cs
@@ -106,7 +106,6 @@ namespace ArsonistMod.SkillStates.Arsonist.Secondary
             else
             {
                 Destroy(effectObj);
-                Destroy(this);
             }
 
         }
